f8ef137d01c1b33e62437ec61b4ba8a494ed2075
Cash <32471210+pickfr@users.noreply.github.com>
2023-08-09 18:02:45 +0100

    Add operator function to combine individual funcs
    Tested and working

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 
 
+let x; // first number
+let y; // second number
+let op; // operator
+let result;
 
 
+
+function operate(operator, x ,y){
+
+    if (operator === "+"){result = add(x,y);}
+    if (operator === "-"){result = subtract(x,y);}
+    if (operator === "*"){result = multiply(x,y);}
+    if (operator === "/"){result = divide(x,y);}
+
+    return result;
+
+}
 
 function add(x,y){
 
