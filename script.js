ed6cf7805fb9bd49e0b76740580701f72689e86a
Cash <32471210+pickfr@users.noreply.github.com>
2023-08-10 03:38:29 +0100

    Add some display functionality
    Not working as intended so going to try a refactor.

--- script.js
+++ script.js
@@ -3,14 +3,59 @@
 let x; // first number
 let y; // second number
 let op; // operator
-let result;
-let display = 0;
+let input = 0;
+
+let display;
+let displayBuffer = 0;
+let numpad;
+let opPad;
+
+
+
+//Temporary program start
+buildUI();
+setUpNumpad();
+
 
 function buildUI(){
 
-    
+    display = document.getElementById("display");
+    display.textContent = displayBuffer
 
 }
+
+function refreshUI(){
+
+    display.textContent = displayBuffer;
+
+}
+
+function setUpNumpad(){
+
+    numpad = document.getElementsByClassName("numpadKey")
+    
+    for(i = 0; i < numpad.length; i++){
+
+        numpad[i].addEventListener("click", (e) => {
+            
+           input = e.target.getAttribute("id");
+           if(result === 0){displayBuffer = input;}
+           result += input;
+           refreshUI();
+
+        })
+
+    }
+}
+
+function setUpOpPad(){
+
+    opPad = document.getElementsByClassName("operator")
+
+}
+
+
+
 
 function operate(operator, x ,y){
 
