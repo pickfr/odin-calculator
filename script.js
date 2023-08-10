7ff935d4444ae4c9ca18b4458207f5c2a785df00
Cash <32471210+pickfr@users.noreply.github.com>
2023-08-10 14:31:41 +0100

    Implement storing numbers on operation
    I think this won't work. Need to store the numbers on
    every key press.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 
-let x; // first number
-let y; // second number
-let op; // operator
-let input = 0;
+let lastNumber = 0; // first number
+let currentNumber = 0; // second number
+let result = 0;
+let op = null; // operator
+let input = null;
+let onNextClickClearDisplay = false;
 
 let display;
 let displayBuffer = 0;
@@ -13,14 +15,24 @@ let opPad;
 
 
 //Temporary program start
-buildUI();
+buildResetUI();
 setUpNumpad();
+setUpOpPad();
+setUpEqualsButton();
+setUpClearButton();
 
 
-function buildUI(){
+function buildResetUI(){
 
     display = document.getElementById("display");
-    display.textContent = displayBuffer
+    lastNumber = 0;
+    currentNumber = 0;
+    op = null;
+    input = null;
+    onNextClickClearDisplay = false;
+    displayBuffer = 0;
+
+    display.textContent = displayBuffer;
 
 }
 
@@ -30,21 +42,14 @@ function refreshUI(){
 
 }
 
+
+// Button Setup
 function setUpNumpad(){
 
     numpad = document.getElementsByClassName("numpadKey")
     
     for(i = 0; i < numpad.length; i++){
-
-        numpad[i].addEventListener("click", (e) => {
-            
-           input = e.target.getAttribute("id");
-           if(result === 0){displayBuffer = input;}
-           result += input;
-           refreshUI();
-
-        })
-
+        numpad[i].addEventListener("click", (e) => numbPadPushed(e));
     }
 }
 
@@ -52,18 +57,106 @@ function setUpOpPad(){
 
     opPad = document.getElementsByClassName("operator")
 
+    for(i = 0; i < opPad.length; i++){
+        opPad[i].addEventListener("click", (e) => opPadPushed(e));
+    }
+
+}
+
+function setUpEqualsButton(){
+
+    equalsButton = document.getElementById("equals");
+
+    equalsButton.addEventListener("click", (e) => equalsPushed(e));
+
+}
+
+function setUpClearButton(){
+
+    clearButton = document.getElementById("clear");
+
+    clearButton.addEventListener("click", () => clearPushed());
+
 }
 
 
 
 
+
+// Button Programs
+
+function numbPadPushed(e){
+
+    input = e.target.getAttribute("id");
+
+    if(onNextClickClearDisplay === true){ 
+        displayBuffer = input;
+        onNextClickClearDisplay = false;
+    }
+
+    console.log(typeof displayBuffer);
+
+    if(displayBuffer == 0 || (displayBuffer == 0 && input == 0)){
+     displayBuffer = input;
+     onNextClickClearDisplay = false;
+     }else{ displayBuffer += input; }
+    
+    refreshUI();
+
+}
+
+function opPadPushed(e){
+
+    input = e.target.getAttribute("id");
+
+    if(!currentNumber && displayBuffer != 0){
+
+        currentNumber = parseInt(displayBuffer);
+        displayBuffer = 0;
+        refreshUI();
+
+    } else {
+
+        lastNumber = currentNumber;
+        currentNumber = parseInt(displayBuffer);
+        displayBuffer = operate(input, lastNumber, currentNumber);
+        currentNumber = operate(input, lastNumber, currentNumber);
+        result = operate(input, lastNumber, currentNumber);
+        displayBuffer = 0;
+        refreshUI();
+    }
+
+
+
+}
+
+function equalsPushed(e){
+
+    displayBuffer = result;
+    refreshUI();    
+    console.log(result);
+
+}
+
+function clearPushed(){
+
+    buildResetUI();
+
+}
+
+
+
 function operate(operator, x ,y){
+
+    let result;
+    x = parseInt(x);
+    y = parseInt(y);
 
     if (operator === "+"){result = add(x,y);}
     if (operator === "-"){result = subtract(x,y);}
     if (operator === "*"){result = multiply(x,y);}
     if (operator === "/"){result = divide(x,y);}
-
+    console.log(`${x} ${operator} ${y} = ${result}`);
     return result;
 
 }
